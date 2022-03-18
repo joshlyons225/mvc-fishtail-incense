505a80da712331e405c7ddbfd509aab18bae2731
joshlyons225 <95392565+joshlyons225@users.noreply.github.com>
2022-03-18 16:22:40 -0500

    updated index.js files in controller directory to include router

---
+++ controllers/index.js
@@ -0,0 +1,7 @@
+const router = require("express").Router();
+
+// write const require path for all routes in api folder
+
+// write router.use statements for all routes in api folder
+
+module.exports = router;
