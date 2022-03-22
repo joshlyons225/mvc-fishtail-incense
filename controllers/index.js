410745f775c647c76f78718150e825a06169a913
joshlyons225 <95392565+joshlyons225@users.noreply.github.com>
2022-03-22 15:32:29 -0500

    updated main routes.js files

--- controllers/index.js
+++ controllers/index.js
@@ -1,7 +1,15 @@
 const router = require("express").Router();
 
-// write const require path for all routes in api folder
+const apiRoutes = require("./api");
+const homeRoutes = require("./home-routes.js");
+const dashboardRoutes = require("./dashboard-routes.js");
 
-// write router.use statements for all routes in api folder
+router.use("/api", apiRoutes);
+router.use("/", homeRoutes);
+router.use("/dashboard", dashboardRoutes);
+
+router.use((req, res) => {
+  res.status(404).end();
+});
 
 module.exports = router;
