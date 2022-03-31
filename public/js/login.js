d3de064374156a35aa52f8ae10717423bde2bf03
joshlyons225 <95392565+joshlyons225@users.noreply.github.com>
2022-03-31 14:52:47 -0500

    updated js files in public directory

--- public/js/login.js
+++ public/js/login.js
@@ -1,31 +1,30 @@
-// signup function for new accounts
 async function signupFormHandler(event) {
-  event.preventDefault();
+    event.preventDefault();
 
-  const username = document.querySelector("#username-signup").value.trim();
-  const email = document.querySelector("#email-signup").value.trim();
-  const password = document.querySelector("#password-signup").value.trim();
+    const username = document.querySelector('#username-signup').value.trim();
+    const email = document.querySelector('#email-signup').value.trim();
+    const password = document.querySelector('#password-signup').value.trim();
 
-  if (username && email && password) {
-    const response = await fetch("/api/users", {
-      method: "post",
-      body: JSON.stringify({
-        username,
-        email,
-        password,
-      }),
-      headers: { "Content-Type": "application/json" },
-    });
+    if (username && email && password) {
+        const response = await fetch('/api/users', {
+            method: 'post', 
+            body: JSON.stringify({
+                username,
+                email,
+                password
+            }),
+            headers: { 'Content-Type': 'application/json' }
+        })
 
-    if (response.ok) {
-      console.log("success");
-    } else {
-      alert(response.statusText);
+        if (response.ok) {
+            console.log('success')
+            document.location.replace('/dashboard/')
+        } else {
+            alert(response.statusText);
+        }
     }
-  }
 }
 
-// login function for existing users
 async function loginFormHandler(event) {
   event.preventDefault();
 
@@ -43,16 +42,15 @@ async function loginFormHandler(event) {
     });
 
     if (response.ok) {
-      document.location.replace("/dashboard");
-    } else {
-      alert(response.statusText);
+      setTimeout(() => {
+        document.location.replace("/dashboard");
+        console.log("you are now logged in");
+      }, 100);
     }
+  } else {
+    alert(response.statusText);
   }
 }
 
-document
-  .querySelector(".signup-form")
-  .addEventListener("submit", signupFormHandler);
-document
-  .querySelector(".login-form")
-  .addEventListener("submit", loginFormHandler);
+document.querySelector('.signup-form').addEventListener('submit', signupFormHandler);
+document.querySelector('.login-form').addEventListener('submit', loginFormHandler);
